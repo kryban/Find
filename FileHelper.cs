7010a8d88597b661335eadc4e6a182857270caf1
kryban <kry.ban@gmail.com>
2016-12-08 10:15:38 +0100

    prod wijzigingen

--- FileHelper.cs
+++ FileHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
@@ -13,8 +13,9 @@ namespace Find
             List<string> retval;
             string searchPattern = "*." + fileType;
             try {
+                Console.Write("> Collecting files ");
                 retval = Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories).ToList<string>();
-                Console.Write("> Collected Files ");
+                Console.Write("> Collected Files (" +retval.Count()+")");
             }
             catch(Exception)
             {
@@ -32,6 +33,7 @@ namespace Find
 
             foreach (string path in list)
             {
+                string p = path;
                 string fileOriginal = File.ReadAllText(path);
                 string formatted = fileOriginal
                                     .Substring(fileOriginal.IndexOf("#endif // __DESIGNER_DATA") + 1)
@@ -48,6 +50,22 @@ namespace Find
             }
 
             return matches;
+        }
+
+        public static void ExportResults(string criteria, List<string> resultSetCreation, List<string> resultSetUsage)
+        {
+            string exportfilePath = Directory.GetCurrentDirectory() + "resultsExported.txt";
+
+            File.WriteAllText(exportfilePath, "Find creation and usage results > " + DateTime.Now);
+            File.AppendAllText(exportfilePath, Environment.NewLine + "--------------");
+            File.AppendAllText(exportfilePath,
+                Environment.NewLine + Environment.NewLine + "Creation of \"" + criteria + " :" + Environment.NewLine);
+            File.AppendAllLines(exportfilePath, resultSetCreation);
+            File.AppendAllText(exportfilePath,
+                Environment.NewLine + Environment.NewLine + "Usage of \"" + criteria + " :" + Environment.NewLine);
+            File.AppendAllLines(exportfilePath, resultSetUsage);
+
+            System.Diagnostics.Process.Start(exportfilePath);
         }
     }
 }
